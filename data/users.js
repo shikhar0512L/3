6a60afe76ed5994db80f51af59f9f3927016fbbb
shikhar0512L <74178918+shikhar0512L@users.noreply.github.com>
2022-04-21 14:45:48 +0530

    Add files via upload

---
+++ data/users.js
@@ -0,0 +1,27 @@
+export const USERS = [
+    {
+        user:'Daltonic',
+        image:'',
+    },
+    {
+        user:'Bobby',
+        image:'',
+    },
+    {
+        user:"Sucessssss",
+        image:'',
+    },
+    {
+        user:"Matt",
+        image:'',
+    },
+    {
+        user:"Lukas",
+        image:"https://nice.gg"
+    },
+    {
+        user:'Code With Harry',
+        image:'https://yt3.ggpht.com/ytc/AKedOLT3EnMXtIOvDT4CL7obl0-acSZCBhMuapXBQFksVQ=s176-c-k-c0x00ffffff-no-rj',
+    }
+    
+]
